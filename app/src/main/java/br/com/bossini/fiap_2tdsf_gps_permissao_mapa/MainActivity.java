390b39f2f7b857da3fe39268ca3ff94c53af0010
Fábrica de Software USJT <fabricadesoftwareusjt@gmail.com>
2019-04-11 09:51:53 -0300

    Terminamos o pdf.

--- app/src/main/java/br/com/bossini/fiap_2tdsf_gps_permissao_mapa/MainActivity.java
+++ app/src/main/java/br/com/bossini/fiap_2tdsf_gps_permissao_mapa/MainActivity.java
@@ -2,11 +2,14 @@ package br.com.bossini.fiap_2tdsf_gps_permissao_mapa;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
@@ -15,16 +18,24 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private LocationManager locationManager;
     private LocationListener locationListener;
     private static final int REQUEST_PERMISSION_GPS = 1001;
+
+    private double lat;
+    private double lon;
+
+    private TextView locationTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        locationTextView = findViewById(R.id.locationTextView);
         locationManager = (LocationManager)
                 getSystemService(Context.LOCATION_SERVICE);
         Toolbar toolbar = findViewById(R.id.toolbar);
@@ -34,15 +45,35 @@ public class MainActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Uri gmmIntentUri =
+                        Uri.parse(
+                                String.format(
+                                        "geo:%f, %f?q=%s",
+                                        lat,
+                                        lon,
+                                        getString(
+                                                R.string.busca
+                                        )
+                                )
+                        );
+                Intent intent =
+                        new Intent (Intent.ACTION_VIEW, gmmIntentUri);
+                intent.setPackage("com.google.android.apps.maps");
+                startActivity(intent);
+
             }
         });
 
         locationListener = new LocationListener() {
             @Override
             public void onLocationChanged(Location location) {
-
+                lat = location.getLatitude();
+                lon = location.getLongitude();
+                double alt = location.getAltitude();
+                String t =
+                        String.format(
+                          "Lat: %f, Long: %f, Alt: %f",lat, lon, alt);
+                locationTextView.setText(t);
             }
 
             @Override
@@ -63,6 +94,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        locationManager.removeUpdates(locationListener);
+    }
+
+    @Override
     protected void onStart() {
         super.onStart();
         if (ActivityCompat.checkSelfPermission(
@@ -76,11 +113,35 @@ public class MainActivity extends AppCompatActivity {
         }
         else{
             String [] permissions = {Manifest.permission.ACCESS_FINE_LOCATION};
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_PERMISSION_GPS);
+            ActivityCompat.requestPermissions(this,
+                    permissions, REQUEST_PERMISSION_GPS);
         }
 
     }
 
-
-
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        if (requestCode == REQUEST_PERMISSION_GPS){
+            if (grantResults.length >= 1 &&
+                    grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                if (ActivityCompat.checkSelfPermission(
+                        this,
+                        Manifest.permission.ACCESS_FINE_LOCATION
+                ) == PackageManager.PERMISSION_GRANTED){
+                    locationManager.requestLocationUpdates(
+                            LocationManager.GPS_PROVIDER,
+                            2000,
+                            0,
+                            locationListener
+                    );
+                }
+            }
+            else{
+                Toast.makeText(this, getString(R.string.sem_gps_nao_rola),
+                        Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
 }
